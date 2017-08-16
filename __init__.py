21ba88306c5c77dbdddccfb661f18dacd4c9e45c
?ngel ?lvarez <angel@nan-tic.com>
2017-08-16 17:19:21 +0200

    fix scenario and flakes issues

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .purchase import *
+from . import purchase
+
 
 def register():
     Pool.register(
-        Purchase,
+        purchase.Purchase,
         module='purchase_confirmed2quotation', type_='model')
